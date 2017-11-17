22780bd8be977541c9d17131e51d295331acfb1e
Evan <epbarger@gmail.com>
2017-11-16 19:48:27 -0500

    comment out browser_timezone_rails, use protocol-less urls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,5 @@
 //= require turbolinks
 // require jquery.cookie
 //= require jstz
-//= require browser_timezone_rails/set_time_zone
+// require browser_timezone_rails/set_time_zone
 //= require_tree .
